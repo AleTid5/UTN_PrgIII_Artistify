680e230dda66992d6c3654db3fc9efe9991b9078
Alejandro Tidele <alejandro@dynamonet.com.ar>
2019-06-24 11:36:21 -0300

    Se agrega instalador para Ubuntu

---
+++ WebApp/Frontend/U-Installer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo Instalando paquetes esenciales para utilizar el sistema...
+npm i
+echo - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+echo Paquetes esenciales instalados exitosamente!
+echo Levantando el servidor...
+npm run start
